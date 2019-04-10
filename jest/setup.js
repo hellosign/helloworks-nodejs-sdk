5ce35a0571bab9284aec93a5fbe711084e802bad
Nathan Buchar <nathan.buchar@hellosign.com>
2019-04-10 10:59:42 -0700

    chore: Do not use @example.com to test emails
    It's causing HW servers to show a misleading number of email bounces. Oops!

--- jest/setup.js
+++ jest/setup.js
@@ -2,6 +2,7 @@ const HelloWorks = require('../');
 
 const testApiKeyId = process.env.HW_API_KEY_ID;
 const testApiKeySecret = process.env.HW_API_KEY_SECRET;
+const testEmailAddress = process.env.HW_TEST_EMAIL_ADDRESS;
 const testWorkflowId = process.env.HW_TEST_WORKFLOW_ID;
 const testCompletedInstanceId = process.env.HW_TEST_COMPLETED_INSTANCE_ID;
 
@@ -18,6 +19,7 @@ const client = new HelloWorks({
 
 global.testApiKeyId = testApiKeyId;
 global.testApiKeySecret = testApiKeySecret;
+global.testEmailAddress = testEmailAddress;
 global.testWorkflowId = testWorkflowId;
 global.testCompletedInstanceId = testCompletedInstanceId;
 global.client = client;
